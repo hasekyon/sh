5c0d3d68499867e3a03d609c41d53e964206bc80
root <root@localhost.localdomain>
2014-04-26 08:42:44 +0900

    iptables commit

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 sh
 ==
+iptables.sh
+
+CentOS syslog setting
+/etc/rsyslog.conf
+
+kern.debug /var/log/iptables.log
+
+/etc/init.d/rsyslog restart
--- iptables.sh
+++ iptables.sh
@@ -1,1 +1,23 @@
 #!/bin/bash
+
+#backup
+iptables-save > ~/iptables.bak
+
+/etc/init.d/iptables stop
+
+#rule delete
+iptables -F
+
+#ssh setting
+SSH_ACCEPT_IP[0]=192.168.56.1
+
+for IP in $SSH_ACCEPT_IP
+do
+iptables -A INPUT -s $IP -p tcp --dport 22 -j ACCEPT
+done
+
+iptables -A INPUT -p tcp --dport 22 -j LOG --log-level debug --log-prefix [IPTABLES_SSH_DROP]:
+iptables -A INPUT -p tcp --dport 22 -j DROP
+
+/etc/init.d/iptables save
+/etc/init.d/iptables start
